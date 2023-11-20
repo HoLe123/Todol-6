1a4b1c53915cd3ebede335e96c148e4221c41c1e
HoLe123 <151480066+HoLe123@users.noreply.github.com>
2023-11-20 09:28:53 +0700

    todolist

---
+++ surprised-donuts/App.js
@@ -0,0 +1,39 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+     Name: Ho Viet Le
+     SDT: 0344386810
+     Email: 220501014@student.bdu.edu.vn
+     MSSV :220501014
+     Sở thích : Game, nghe nhạc
+      </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
